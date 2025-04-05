52a9f14733a543e4500461f450c4486ec3342197
YounsooLim <tomandlim@gmail.com>
2025-04-05 23:46:42 +0900

    feat: add aRandom value from geometry

--- three/shaders/shaders/test/vertex.glsl
+++ three/shaders/shaders/test/vertex.glsl
@@ -3,11 +3,13 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
 attribute vec3 position;
+attribute float aRandom;
 
 void main()
 {
 	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-	modelPosition.z += sin(modelPosition.x * 20.0) * 0.1;
+	// modelPosition.z += sin(modelPosition.x * 20.0) * 0.1;
+	modelPosition.z += aRandom * 0.1;
 
 	vec4 viewPotision = viewMatrix * modelPosition;
 	vec4 projectedPosition = projectionMatrix * viewPotision;
