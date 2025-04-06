866c04e410658d31e5ba6a6c51f5f449550116b7
YounsooLim <tomandlim@gmail.com>
2025-04-06 15:07:46 +0900

    feat(shader): add uniforms to control values

--- three/shaders/shaders/test/vertex.glsl
+++ three/shaders/shaders/test/vertex.glsl
@@ -1,6 +1,8 @@
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
+uniform vec2 uFrequency;
+uniform float uTime;
 
 attribute vec3 position;
 attribute float aRandom;
@@ -10,8 +12,9 @@ varying float vRandom;
 void main()
 {
 	vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-	// modelPosition.z += sin(modelPosition.x * 20.0) * 0.1;
-	modelPosition.z += aRandom * 0.1;
+	modelPosition.z += sin(uTime + modelPosition.x * uFrequency.x) * 0.1;
+	modelPosition.z += sin(modelPosition.y * uFrequency.y) * 0.1;
+	// modelPosition.z += aRandom * 0.1;
 
 	vec4 viewPotision = viewMatrix * modelPosition;
 	vec4 projectedPosition = projectionMatrix * viewPotision;
