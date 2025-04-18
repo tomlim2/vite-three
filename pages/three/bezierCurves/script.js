a059da2eef885208240acc8f37ed9aaed5c5e1ae
YounsooLim <tomandlim@gmail.com>
2025-04-18 23:27:36 +0900

    feat: add spiral

--- pages/three/bezierCurves/script.js
+++ pages/three/bezierCurves/script.js
@@ -2,6 +2,27 @@ import * as THREE from "three/webgpu";
 import { ParametricGeometry } from "three/examples/jsm/Addons.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { DoubleSide } from "three";
+import {
+  vec4,
+  vec3,
+  uniform,
+  equirectUV,
+  positionLocal,
+  Fn,
+  time,
+  mul,
+  div,
+  add,
+  sub,
+  floor,
+  fract,
+  uv,
+  sin,
+  cos,
+  abs,
+  texture,
+  oneMinus
+} from "three/tsl";
 
 const width = window.innerWidth,
   height = window.innerHeight;
@@ -9,7 +30,8 @@ const width = window.innerWidth,
 // init
 
 const camera = new THREE.PerspectiveCamera(35, width / height, 0.01, 100);
-camera.position.set(0, -1, 20);
+camera.position.set(2, 2, 20);
+camera.lookAt(0, 0, 0);
 
 const controls = new OrbitControls(camera, document.body);
 controls.enableDamping = true;
@@ -29,7 +51,7 @@ function bezier(a, b, c, d, t) {
   );
 }
 function getRadius(t) {
-  return bezier(40, 10, 1, 1, t);
+  return bezier(70, 1, 1, 1, t);
 }
 
 const geometry = new ParametricGeometry(
@@ -37,18 +59,67 @@ const geometry = new ParametricGeometry(
     let r = getRadius(v);
     let x = r * Math.sin(u * Math.PI * 2);
     let y = r * Math.cos(u * Math.PI * 2);
-    let z = (v - 0.5) * 12;
+    let z = (v - 0.5) * 15;
+
     target.set(x, y, z);
   },
   325,
   325
 );
 
-const material = new THREE.MeshNormalMaterial();
+let playhead = uniform(0.0);
+
+let myMap = new THREE.TextureLoader().load("/textures/pattern-random-01.jpg");
+myMap.colorSpace = THREE.SRGBColorSpace;
+myMap.minFilter = THREE.LinearMipMapLinearFilter;
+myMap.magFilter = THREE.LinearFilter;
+myMap.wrapS = THREE.RepeatWrapping;
+myMap.wrapT = THREE.RepeatWrapping;
+myMap.repeat.set(1, 1);
+
+const material = new THREE.MeshPhysicalNodeMaterial();
+material.color.set(0x000099);
+material.roughness = 0.5;
+material.metalness = 0.5;
 material.side = DoubleSide;
+
+let caluculated = Fn(() => {
+	let row = abs(floor(fract(uv().y.add(playhead)).mul(20)));
+	let randomValue = fract(sin(row.mul(123)).mul(345678.123));
+  
+	let newuv = uv().toVar();
+	newuv.x.mulAssign(7);
+	newuv.y.add(playhead);
+	newuv.x = fract(newuv.x);
+	newuv.y = newuv.y.mul(1);
+
+	let rowSpeed = randomValue.mul(15).add(0.5);
+	newuv.x.addAssign(playhead.mul(rowSpeed));
+	
+  //   return vec4(newuv, 0, 1);
+  return texture(myMap, newuv).r.oneMinus();
+  });
+
+material.colorNode = caluculated();
+material.roughnessNode = caluculated();
+
 const mesh = new THREE.Mesh(geometry, material);
 mesh.rotation.x = -1;
 scene.add(mesh);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(-10, -2, 4);
+light.castShadow = true;
+scene.add(light);
+scene.add(new THREE.AmbientLight(0xffffff, 2));
+const light2 = new THREE.DirectionalLight(0xffffff, 0.01);
+light2.position.set(0, 0, -1);
+light2.castShadow = true;
+scene.add(light2);
+const light3 = new THREE.PointLight(0xffffff, 100);
+light3.position.set(5, 2, -2);
+light3.castShadow = true;
+scene.add(light3);
 
 const renderer = new THREE.WebGPURenderer({ antialias: true });
 renderer.setSize(width, height);
@@ -60,6 +131,6 @@ document.body.appendChild(renderer.domElement);
 function animate(time) {
   //   mesh.rotation.x = time / 2000;
   //   mesh.rotation.y = time / 1000;
-
+  playhead.value = - time / 15000;
   renderer.renderAsync(scene, camera);
 }
